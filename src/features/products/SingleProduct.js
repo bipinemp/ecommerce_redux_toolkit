025f9269bfad9be581b0388b1bfe1886fcc04213
Bipin Bhandari <bhandaribipin3.39@gmail.com>
2022-12-09 22:21:39 +0545

    maded singleProduct flex column

--- src/features/products/SingleProduct.js
+++ src/features/products/SingleProduct.js
@@ -15,31 +15,29 @@ function SingleProduct() {
       </button>
 
       {products.view.length > 0 ? (
-        <div className="w-9/12 sm:w-fit sm:m-4 px-10 py-10 md:px-2 md:py-2 flex justify-center items-center bg-gray-50 border-2 border-gray-300 rounded-2xl  ml-auto mt-0 mb-10 mr-auto">
+        <div className="w-9/12 sm:w-fit sm:m-4 px-5 py-5 md:px-2 md:py-2 flex justify-center items-center bg-gray-50 border-2 border-gray-300 rounded-2xl  ml-auto mt-0 mb-10 mr-auto">
           {products.view.map((prod) => (
-            <div className="md:flex-col w-full h-full flex  justify-between  gap-10 px-10 py-10 md:px-0 md:py-0">
+            <div className="md:flex-col w-full h-full flex flex-col  justify-between  gap-10 px-10 py-10 md:px-0 md:py-0">
               <div className="flex flex-col justify-center items-center gap-10">
                 <img
                   src={prod.image}
-                  className="mix-blend-darken w-96 md:w-32"
+                  className="mix-blend-darken w-40 md:w-32"
                   alt="singleProduct"
                 />
               </div>
               <div className="flex flex-col justify-center gap-3 ">
-                <p className="font-bold text-3xl md:text-xl md:text-center ">
+                <p className="font-bold text-3xl md:text-xl text-center ">
                   {prod.title}
                 </p>
-                <p className="text-red-600 font-bold text-2xl md:text-center">
+                <p className="text-red-600 font-bold text-2xl text-center">
                   Price : ${prod.price}
                 </p>
-                <p className="text-gray-700 md:text-center">
-                  {prod.description}
-                </p>
-                <p className="md:text-center">
+                <p className="text-gray-700 text-center">{prod.description}</p>
+                <p className="text-center">
                   <b>Category :</b> <br />
                   {prod.category}
                 </p>
-                <p className="md:text-center">
+                <p className="text-center">
                   <b>Rating :</b> <br />
                   {prod.rating.rate}
                 </p>
